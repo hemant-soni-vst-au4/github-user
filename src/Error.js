2a3027ae8b9709d4b0e69cbc1f29a79228fcaea4
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-10 22:59:49 +0530

    error component with link to go back

--- src/Error.js
+++ src/Error.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
     return (
         <div>
             <h1>Error</h1>
+            <h3>sorry, the page you tried cannot be found</h3>
+             <Link to="/" className="btn">Back home</Link>
         </div>
     )
 }
